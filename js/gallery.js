aad51c1fc527d5b1284633dbc520de4df4f143b9
MarinaKorn <gecagem840@gmail.com>
2025-04-09 22:55:47 +0200

    1

---
+++ js/gallery.js
@@ -0,0 +1,61 @@
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+];
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = images
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+
+galleryEl.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  const isImg = event.target.classList.contains('gallery-image');
+  if (!isImg) return;
+
+  const originalSrc = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${originalSrc}" width="800" height="600">
+  `);
+
+  instance.show();
+}
